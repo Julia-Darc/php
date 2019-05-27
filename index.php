14b83a3c31ebcf83ebae13a0a6cdf40e3b13480f
Julia Darc <juliadarcviana@gmail.com>
2019-05-27 01:17:23 -0300

    adicionei um echo

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Olá batman";
 	echo "Olá Alfred";
-=======
-	echo "Hello Batman";
-	echo "Hello Alfred"
->>>>>>> 6a88efd7473df85cbfd14b5c8509e98e84a8d49f
+	echo "socorro";
 
 ?>
